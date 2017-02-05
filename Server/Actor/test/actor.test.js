b96a78ceb0dcab95a4e60d8d52b7b4c527cb3a81
vu hoang ha <vu hoang ha>
2017-02-05 15:37:35 +0700

    full test CRUD actor

--- Server/Actor/test/actor.test.js
+++ Server/Actor/test/actor.test.js
@@ -9,41 +9,111 @@ const testcoverage = require('../test-coverage/util');
 
 chai.use(chaiHttp);
 
-const actor = {
-    acId: 1,
-    quantityPost: 0,
-    acName: 'vuhoangha',
-};
-
 const getAll = () => {
-    it('it should GET all the actors', done => {
+    it('it should GET ALL the actors', done => {
+        const record = {
+            acId: 1,
+            quantityPost: 0,
+            acName: 'vuhoangha',
+        };
+
         chai.request(server)
-            .get('/actor')
+            .post('/actor')
+            .send(record)
             .end((err, res) => {        // eslint-disable-line
-                res.should.have.status(200);
-                console.log(res.body);
-                res.body.should.be.a('array');
-                res.body.length.should.be.eql(1);
-                done();
+                should.not.exist(err);
+                if (!err) {
+                    chai.request(server)
+                        .get('/actor')
+                        .end((err, res) => {        // eslint-disable-line
+                            res.should.have.status(200);
+                            console.log(res.body);
+                            res.body.should.be.a('array');
+                            res.body.length.should.be.eql(1);
+                            done();
+                        });
+                }
             });
     });
 };
 
 const post = () => {
-    it('it should not POST a actor without acName field', done => {
+    it('it should POST a actor', done => {
+        const record = {
+            acId: 2,
+            quantityPost: 20,
+            acName: 'hoangthihuong',
+        };
+
         chai.request(server)
             .post('/actor')
-            .send(actor)
-            .end((err, res) => {
-                console.log('Loi : ');
-                console.log(err);
-                console.log('Response : ');
-                console.log(res.text);
+            .send(record)
+            .end((err, res) => {        // eslint-disable-line
                 res.should.have.status(200);
                 res.text.should.be.a('string');
                 res.text.should.equal('OK');
-                describe('/GET actor', getAll);
                 done();
+            });
+    });
+};
+
+const put = () => {
+    it('it should PUT actor', done => {
+        const record = {
+            acId: 3,
+            quantityPost: 50,
+            acName: 'vukhuongduy',
+        };
+        const key = {
+            acId: 3,
+        };
+
+        chai.request(server)
+            .post('/actor')
+            .send(record)
+            .end((err, res) => {        // eslint-disable-line
+                should.not.exist(err);
+                if (!err) {
+                    chai.request(server)
+                        .put(`/actor?key=${JSON.stringify(key)}`)
+                        .send(record)
+                        .end((err, res) => {        // eslint-disable-line
+                            res.should.have.status(200);
+                            res.text.should.be.a('string');
+                            res.text.should.equal('OK');
+                            done();
+                        });
+                }
+            });
+    });
+};
+
+const del = () => {
+    it('it should DELETE actor', done => {
+        const record = {
+            acId: 4,
+            quantityPost: 50,
+            acName: 'vuvandoanh',
+        };
+        const key = {
+            acId: 4,
+        };
+
+        chai.request(server)
+            .post('/actor')
+            .send(record)
+            .end((err, res) => {        // eslint-disable-line
+                should.not.exist(err);
+                if (!err) {
+                    chai.request(server)
+                        .delete(`/actor?key=${JSON.stringify(key)}`)
+                        .end((err, res) => {        // eslint-disable-line
+                            res.should.have.status(200);
+                            res.text.should.be.a('string');
+                            res.text.should.equal('OK');
+                            done();
+                        });
+                }
             });
     });
 };
@@ -61,13 +131,16 @@ const coverage = () => {
     });
 };
 
-describe('Actors Test API', () => {
+describe('\n\n\nActors Test API', () => {
     beforeEach(done => {
         query.deleteAll(done);
     });
-    describe('/POST actor', post);
+    describe('\n\n/GET ALL actor', getAll);
+    describe('\n\n/POST actor', post);
+    describe('\n\n/PUT actor', put);
+    describe('\n\n/DELETE actor', del);
 });
 
-describe('Actors Test Function', () => {
-    describe('/Coverage code', coverage);
+describe('\n\n\nActors Test Function', () => {
+    describe('\n\n/Coverage code', coverage);
 });
